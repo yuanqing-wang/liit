66d49624e711b5f3a1f51ab5e3b8a234dd7a24a7
Yuanqing Wang <wangyq@wangyq.net>
2024-02-15 23:52:16 -0800

    script

--- scripts/uspto/run.py
+++ scripts/uspto/run.py
@@ -1,26 +1,52 @@
 import token
 import torch
+import pandas as pd
 from dataset import USPTO
-from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
+from datasets import load_dataset, Dataset
 
 def run():
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    model_name = "gpt2"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    model = AutoModelForCausalLM.from_pretrained(model_name)
 
-    dataset = USPTO(
-        path="uspto.csv",
+    df = pd.read_csv("uspto.csv")[:1000]
+    dataset = Dataset.from_pandas(df)
+
+    def tokenize_function(examples):
+        return tokenizer(
+            examples["reaction"], 
+            examples["paragraph"],
+            padding="max_length", 
+            truncation=True
+        )
+
+    tokenized_datasets = dataset.map(tokenize_function, batched=True, batch_size=4)
+
+    # split the dataset into train and validation
+    tokenized_datasets = tokenized_datasets.train_test_split(test_size=0.2)
+
+    training_args = Seq2SeqTrainingArguments(
+        output_dir="./results",
+        evaluation_strategy="epoch",
+        learning_rate=1e-4,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
+        num_train_epochs=10,
+        save_total_limit=2,
+    )
+
+    trainer = Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["test"],
         tokenizer=tokenizer,
     )
 
-    reaction, smiles, paragraph = dataset.get(0)
-    question, text = reaction, smiles
-    
-    model = GPT2LMHeadModel.from_pretrained("gpt2")
-    model.eval()
-    # question, text = "Who was Jim Henson?", "Jim Henson was a nice puppet"
-    inputs = tokenizer(question, text, return_tensors="pt")
-    outputs = model.generate(**inputs)
-    outputs = tokenizer.decode(outputs[0], skip_special_tokens=False)
-    print(outputs)
+    trainer.train()
 
     
     
